78ee68aa4fefbc7fdfd886d1c9572f2264e78fb9
Richard Yan <richardyan314@foxmail.com>
2019-04-18 23:04:29 -0400

    rewrite with mutually recursive high order functions

--- printTree.py
+++ printTree.py
@@ -1,3 +1,5 @@
+# python printTree.py > /d/My\ Document/Documents/blog/source/_posts/Library-of-Ashurbanipal/tree.json
+
 import sys
 sys.stdout.reconfigure(encoding='utf-8')
 
@@ -9,17 +11,16 @@ import json
 #import re
 #lcc_re = re.compile("(([A-Z]+(\\.[A-Z0-9]*)*))( - [^-]*)+")
 
-def traverse_dir(root, fdir, ffile, level):
+def iter_dir(root, fdir, ffile, level):
   # do folders first
   for dir in root.iterdir():
     if dir.is_dir():
-      if fdir(dir, level):
-        traverse_dir(dir, fdir, ffile, level+1)
+      yield fdir(dir, level)
 
   # then do files
   for file in root.iterdir():
     if file.is_file():
-      ffile(file, level)
+      yield ffile(file, level)
 
 
 def is_archive(dir):
@@ -41,47 +42,21 @@ def print_file(file, level):
   print('{}{}'.format(subindent, file.parts[-1]))
 
 
-def add2dict():
-  d = list()
-  last_level = 0
-  stk = [d]
-  
-  def add2dict_dir(dir, level):
-    nonlocal last_level
-    
-    isArchive, dirname = is_archive(dir)
-    print_dir(dir, level)
-    if level > last_level:
-      stk.append(stk[-1][-1]["children"])
-      last_level = level
-      
-    while level < last_level:
-      stk.pop()
-      last_level -= 1
-    print(len(stk))
-    stk[-1].append({"name": dirname, "children": list()})
-    return not isArchive
-    
-  def add2dict_file(file, level):
-    pass
-  
-  def get_dict():
-    return d
-    
-  return add2dict_dir, add2dict_file, get_dict
-  
+def dir2Tree(dir, level):
+  isArchive, dirname = is_archive(dir)
+  if isArchive:
+    return dirname
+  else:
+    return {"name": dirname, "children": [item for item in iter_dir(dir, dir2Tree, file2Tree, level+1)]}
+
+
+def file2Tree(file, level):
+  return file.parts[-1]
+
+
 if __name__ == "__main__":
   p = path.Path(os.path.dirname(os.path.realpath(__file__)))
-  #traverse_dir(
-  #  p,
-  #  print_dir,
-  #  print_file,
-  #  0)
-    
-  add2dict_dir, add2dict_file, get_dict = add2dict()
-  traverse_dir(
-    p,
-    add2dict_dir,
-    add2dict_file,
-    0)
-  print(json.dumps(get_dict(), indent=4))
+  print(json.dumps(
+    [item for item in iter_dir(p, dir2Tree, file2Tree, 0)
+          if not ((item["name"] if isinstance(item, dict) else item) in [".git", ".gitignore", __file__])],
+    indent=4, ensure_ascii=False))
